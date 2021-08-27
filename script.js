f74245fe5c415e223d7a594130fad57c7c0a7440
caroline arnoult <carolinearnoult@hotmail.fr>
2021-08-27 20:46:55 +0200

    premier essai js

--- script.js
+++ script.js
@@ -1,24 +1,12 @@
-btnRoll = document.querySelector('.btn-roll');
-
-btnRoll.addEventListener('click', ()  => {
-    if(gamePlaying) {
-        // 1. Random number
-        let dice = Math.floor(Math.random() * 6) + 1;
-
-        //2. Display the result
-        let diceDOM = document.querySelector('.dice');
-        diceDOM.style.display = 'block';
-
-        //3. Update the round score IF the rolled number was NOT a 1
-        if else (dice !== 1) {
-            //Add score
-            roundScore += dice;
-            document.querySelector('#current-' + activePlayer).textContent = roundScore;
-        } else {
-            //Next player
-            nextPlayer();
-        }
-    }    
-});
-
+const playingGame = document.querySelector('.btn-roll');
+playingGame.addEventListener('click', (e) => {
+    if (playingGame) {
+        const dice = Math.floor(Math.random() * 6) + 1;
+        const diceDom = document.querySelector('.dice');
+        diceDom.style.display = 'block';
+        diceDom.src = 'face-' + dice + '.png';
+    } else {
+         NextPlayer()
+    }
+})
 
